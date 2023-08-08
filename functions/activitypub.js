967777d25bb895a2a7a93aa54535dbe94eb23170
Gordon Pedersen <death.au@death.id.au>
2023-08-09 09:32:56 +1000

    The beginnings of an ActivityPub Inbox endpoint

--- functions/activitypub.js
+++ functions/activitypub.js
@@ -1,3 +1,7 @@
+const { Octokit } = require("@octokit/rest");
+const site = require("../www/_data/site");
+
+
 const options = {
   "functionsDirectory":"functions",
   "dataDirectory":"www/_data",
@@ -5,14 +9,59 @@ const options = {
   "tokenEndpoint":"https://tokens.indieauth.com/token"
 };
 
+const getContent = async (path) => {
+  const { data } = await octo.rest.repos.getContent({
+    owner: site.GITHUB_USER,
+    repo: site.GITHUB_REPO,
+    path: path,
+    ref: `heads/activitypub`
+  });
+
+  return Buffer.from(data.content, "base64").toString("utf-8");
+}
+
+const validateContentType = (contentType) => {
+  return contentType === 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+      || contentType === 'application/activity+json'
+}
+
+const processInboxPost = async (event) => {
+  if(validateContentType(event.headers["content-type"])) {
+    return {
+      statusCode: 501,
+      body: JSON.stringify(event)
+    }
+  }
+  else return {
+    statusCode: 400,
+    body: 'Bad Request — Content-Type must be \'application/ld+json; profile="https://www.w3.org/ns/activitystreams"\' (\'application/activity+json\' is also acceptable)'
+  }
+}
+
+const processInboxRequest = async (event) => {
+  if(event.httpMethod === "GET") {
+    content = await getContent(options.dataDirectory + '/inbox.json')
+    return {
+      statusCode: 200,
+      body: content
+    }
+  }
+  else if(event.httpMethod === "POST") {
+    return await processInboxPost(event)
+  }
+  else {
+    return {
+      statusCode: 405,
+      body: 'Method not allowed. Only GET and POST accepted.'
+    }
+  }
+}
+
 exports.handler = async function (event) {
 
   switch (event.path.split("/")[2]) {
     case "inbox":
-      return {
-        statusCode: 501,
-        body: JSON.stringify(event)
-      }
+      return await processInboxRequest(event)
     default:
       return {
         statusCode: 404,
